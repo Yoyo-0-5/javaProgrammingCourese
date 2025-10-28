28a17f4ac56c6bdb76a234c6ac2c9446b27eca4c
詹 宗祐 <d12405009@ems.npu.edu.tw>
2025-10-28 01:22:58 +0000

    AbstractGun

--- javaHomework/hw0923/AbstractGun.java
+++ javaHomework/hw0923/AbstractGun.java
@@ -1,15 +1,45 @@
 package javaHomework.hw0923;
 
 public abstract class AbstractGun {
+
+    private double gunHitRate;
+
+    private double gunHitRateBasedContinuousShooting;
+
+    private int gunDamage;
+
+    private int numberOfShoot;
+
+    public AbstractGun(int gunDamage, double gunHitRate) {
+        this.gunDamage = gunDamage;
+        this.gunHitRate = gunHitRate;
+         
+    }
+
+    public void setGunHitRateBasedContinuousShooting(double n) {
+        gunHitRateBasedContinuousShooting = n;
+    }
+
+    public void setNmberOfShoot(int n) {
+        numberOfShoot = n;
+    }
+
+    public int getNmberOfShoot() {
+        return numberOfShoot;
+    }
+
     public abstract void shoot();
+
     public abstract void fill();
     
-    // 新增：獲得槍枝名稱
     public abstract String getGunName();
     
-    // 新增：更改射擊模式
     public abstract void changeModel(int i);
     
-    // 新增：獲得槍枝傷害力
     public abstract int getDamage();
+
+    
+
+    
+
 }

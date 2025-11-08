9c6b91bd86fdebab3849c05bb709e9fa1810f2e0
詹 宗祐 <d12405009@ems.npu.edu.tw>
2025-11-08 07:16:51 +0000

    新增 TSPProblem 類別以實現旅行商問題的基本結構

---
+++ algorithm/Problem/TSP/TSPProblem.java
@@ -0,0 +1,26 @@
+package algorithm.Problem.TSP;
+
+import java.util.Random;
+
+import algorithm.Problem.Chromosome;
+import algorithm.Problem.Problem;
+
+public class TSPProblem implements Problem<Integer> {
+    Random rand = null;
+
+    @Override
+    public Chromosome<Integer>[] generateChromosomes(int size) {
+        return null;
+    }
+
+    @Override
+    public Integer generateGene(int index) {
+        return null;
+    }
+
+    @Override
+    public void calculateFitness(Chromosome<Integer>[] chromosomes) {
+        
+    }
+
+}
